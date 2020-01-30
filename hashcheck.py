821c0843f05cbeb8c45b019ac87f3aeba6384630
renegadeMind <49590718+renegadeMind@users.noreply.github.com>
2020-01-30 13:24:29 +0100

    Updated the general look and feel
    Updated the look and feel of the script and implemented Windows cmd-commands for testing purposes. Windows is still not supported.

--- hashcheck.py
+++ hashcheck.py
@@ -11,14 +11,14 @@ platform = platform.system()
 fileHash = None
 
 #Header
-print('***********************\n'
+print('\x1b[97;1;49m***********************\n'
 	+ '*                     *\n' 
-	+ '*      HashCheck      *\n'
+	+ '*      \x1b[32;1;49mHashCheck\x1b[97;1;49m      *\n'
 	+ '*                     *\n'
-	+ '***********************\n')
+	+ '***********************\x1b[0m\n')
 
 #Disclaimer
-print('\x1b[37;1;49m Disclaimer: Any installations made on the basis of the result of this software are at the risk of the end-user.\x1b[0m\n\n\n')
+print('\x1b[93;1;49m Disclaimer: Any installations made on the basis of the result of this software are at the risk of the end-user.\x1b[0m\n\n\n')
 
 #If the users operating system is not supported the script informs the user and terminates.
 if platform != 'Windows' and platform != 'Darwin':
@@ -35,7 +35,8 @@ input = input('Input: ')
 if input == '1' or input == 'MD5' or input == 'md5':
 	print('\x1b[91;3;49m MD5 is proven to be unsafe. We recommend that you use SHA-256 if available.\x1b[0m')
 	if platform == 'Windows':
-		print()
+		fileHash = subprocess.check_output('FCIV -md5 ' + fileLocation, shell=True)
+
 	elif platform == 'Darwin':
 		fileHash = subprocess.check_output('md5 ' + fileLocation, shell=True)
 		#cutting the output to md5.length+1 because the string ends with \n
@@ -51,7 +52,8 @@ if input == '1' or input == 'MD5' or input == 'md5':
 elif input == '2' or input == 'SHA-1' or input == 'sha-1' or input == 'SHA1' or input == 'sha1':
 	print('\x1b[91;3;49m SHA-1 is proven to be unsafe. It is recommended that you use SHA-256 if available.\x1b[0m')
 	if platform == 'Windows':
-		print()
+		fileHash = subprocess.check_output('FCIV -sha1 ' + fileLocation, shell=True)
+
 	elif platform == 'Darwin':
 		fileHash = subprocess.check_output('shasum -a 1 ' + fileLocation, shell=True)
 		#Get the first 40 characters of the output string
@@ -75,5 +77,5 @@ else:
 		print('Both SHA-256 hashes are the same. You should be safe to install the software.')
 	else:
 		print('\x1b[91;3;49m The SHA-256 hashes do not match! We recommend deleting the file immediately.\x1b[0m')
-
+#orderly exitting the program
 sys.exit()
