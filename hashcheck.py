1d974448ae19523fc29fd55d79d4c8c0317f249c
renegadeMind <49590718+renegadeMind@users.noreply.github.com>
2020-02-15 11:47:40 +0100

    Spelling and general corrections

--- hashcheck.py
+++ hashcheck.py
@@ -18,7 +18,7 @@ print('\x1b[97;1;49m***********************\n'
 	+ '***********************\x1b[0m\n')
 
 #Disclaimer
-print('\x1b[93;1;49m Disclaimer: Any installations made on the basis of the result of this software are at the risk of the end-user.\x1b[0m\n\n\n')
+print('\x1b[93;1;49m Disclaimer: Opening any file on the basis of the results of this software is at the reisk of the user. We do not take any responsibility.\x1b[0m\n\n\n')
 
 #If the users operating system is not supported the script informs the user and terminates.
 if platform != 'Windows' and platform != 'Darwin':
@@ -28,8 +28,9 @@ if platform != 'Windows' and platform != 'Darwin':
 #Asking the user what kind of hashfunction he would like to test against
 print('What encryption Method has been used?')
 print('[1] MD5     [3] SHA-256')
-print('[2] SHA-1')
+print('[2] SHA-1   [4] Exit')
 input = input('Input: ')
+print('\n')
 
 #MD5 decission-tree
 if input == '1' or input == 'MD5' or input == 'md5':
@@ -45,7 +46,7 @@ if input == '1' or input == 'MD5' or input == 'md5':
 		fileHash = fileHash[:32]
 
 	if providedHash.encode('utf-8') == fileHash:
-		print('Both hashes match. Because the MD5 hash-function has been used, only open the file if you trust the site you downloaded the file from.')
+		print('\x1b[32;3;49m Both hashes match.\x1b[0m Because the MD5 hash-function has been used, only open the file if you trust the site you downloaded the file from.')
 	else:
 		print('\x1b[91;3;49m The hashes do not match! We recommend that you delete the file immediately.\x1b[0m')
 #SHA-1 decission-tree
@@ -60,12 +61,13 @@ elif input == '2' or input == 'SHA-1' or input == 'sha-1' or input == 'SHA1' or 
 		fileHash = fileHash[:40]
 	#Check if both hashes match
 	if providedHash.encode('utf-8') == fileHash:
-		print('Both hashes match: Because the SHA-1 hash-function has beeen used, only open the file if you trust the site you downloaded the file from.')
+		print('\x1b[32;3;49m Both hashes match.\x1b[0m Because the SHA-1 hash-function has beeen used, only open the file if you trust the site you downloaded the file from.')
 	else:
 		print('\x1b[91;3;49m The hashes do not match! We recommend that you delete the file immediately.\x1b[0m')
 #SHA-256 decission-tree
-else:
-	#Here we calculate the SHA checksum for the provided file
+elif input == '3' or input == 'SHA-256' or input == 'sha-256' or input == 'SHA256' or input == 'sha256':
+
+	#Here we calculate the SHA-256 checksum for the provided file
 	if platform == 'Windows':
 		fileHash = subprocess.check_output('certutil -hashfile ' + fileLocation + ' SHA256', shell=True)
 	elif platform == 'Darwin':
@@ -74,8 +76,13 @@ else:
 		fileHash = fileHash[:64]
 
 	if providedHash.encode('utf-8') == fileHash:
-		print('Both SHA-256 hashes are the same. You should be safe to install the software.')
+		print('\x1b[32;3;49m Both SHA-256 hashes are the same. You should be safe to open the file.\x1b[0m')
 	else:
 		print('\x1b[91;3;49m The SHA-256 hashes do not match! We recommend deleting the file immediately.\x1b[0m')
-#orderly exitting the program
+
+else:
+	print('\n\nWe hope we could help you keep your computer safe.')
+	sys.exit()
+
+print('\n\nWe hope we could help you keep your computer safe.')
 sys.exit()
